999332b7b7e07b39f563ef9a3893142e363f83a8
Yonggao <ygdong@thoughtworks.com>
2017-07-05 23:33:19 +0800

    add another example: Foo_provider with provider implemented with spring_boot

---
+++ src/test/java/io/pivotal/microservices/pact/consumer/ConsumerPortTest.java
@@ -0,0 +1,43 @@
+package io.pivotal.microservices.pact.consumer;
+
+import au.com.dius.pact.consumer.*;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.PactFragment;
+import au.com.dius.pact.model.RequestResponsePact;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ConsumerPortTest {
+
+    @Rule
+    public PactProviderRuleMk2 rule = new PactProviderRuleMk2("Foo_Provider", "localhost", 8080, this);
+
+
+
+    @Pact(provider="Foo_Provider", consumer="Foo_Consumer")
+    public RequestResponsePact createFragment(PactDslWithProvider builder) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json;charset=UTF-8");
+
+        return builder.given("Foo_State")
+                .uponReceiving("a request for Foos")
+                .path("/foos")
+                .method("GET")
+                .willRespondWith()
+                .headers(headers)
+                .status(200)
+                .body("[{\"value\":42}, {\"value\":100}]").toPact();
+    }
+
+    @Test
+    @PactVerification("Foo_Provider")
+    public void runTest() {
+        assertEquals(new ConsumerPort("http://localhost:8080").foos(), Arrays.asList(new Foo(42), new Foo(100)));
+    }
+}
